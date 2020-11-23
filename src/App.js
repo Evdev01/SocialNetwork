4f6138a40481845e03f51d7806d8bb30a91b2190
Evdev01 <myzdricov@gmail.com>
2020-11-24 02:03:18 +0300

    Design sketch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
+import BaseCss from "./components/BaseCss/BaseCss";
 
 function App() {
 
     return (
         <div>
-
+            <BaseCss/>
         </div>
     );
 }
