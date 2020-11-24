1e0b0bdd617723ce2a6a8a3c9120a861a346f2d9
Evdev01 <myzdricov@gmail.com>
2020-11-24 18:28:47 +0300

    Added routes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import styles from "./Sidebar.module.css";
+import styles from './Sidebar.module.css'
+import {NavLink} from 'react-router-dom'
 
 function Sidebar() {
 
     return (
-        <div className={styles.sidebar}>
-            <ul className={styles.sidebarUl}>
-                <li>Мой профиль</li>
-                <li>Сообщения</li>
-                <li>Музыка</li>
-                <li>Настройки профиля</li>
-            </ul>
-        </div>
+        <nav className={styles.sidebar}>
+            <div className={styles.sidebarList}>
+                <div className={styles.sidebarItem}>
+                   <NavLink to={'/profile'} activeClassName={styles.activeLink}>Profile</NavLink>
+                </div>
+
+                <div className={styles.sidebarItem}>
+                    <NavLink to={'/dialogs'} activeClassName={styles.activeLink}>Messages</NavLink>
+                </div>
+
+                <div className={styles.sidebarItem}>
+                    <NavLink to={'/users'} activeClassName={styles.activeLink}>Users</NavLink>
+                </div>
+                <div className={styles.sidebarItem}>
+                    <NavLink to={'/settings'} activeClassName={styles.activeLink}>Settings</NavLink>
+                </div>
+            </div>
+        </nav>
     )
 }
 
